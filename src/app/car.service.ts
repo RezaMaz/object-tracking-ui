2fe56537d84d839e2e8414f6ebda49539c5664d3
Mazloom <rmazloom88@gmail.com>
2020-07-23 22:38:23 +0430

    add crud services for car

--- src/app/car.service.ts
+++ src/app/car.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Car} from './model/car';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/v1/car';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getCar(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createCar(car: Car): Observable<any> {
+    return this.http.post(`${this.baseUrl}`, car);
+  }
+
+  updateCar(id: number, value: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deleteCar(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
+  getCarList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/list`);
+  }
 }
