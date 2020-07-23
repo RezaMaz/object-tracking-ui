00caf06795b834e3f08e91487144ef800196657d
Mazloom <rmazloom88@gmail.com>
2020-07-23 15:50:23 +0430

    create list of cars component

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Car} from '../model/car';
+import {CarService} from '../car.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-car-list',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarListComponent implements OnInit {
 
-  constructor() { }
+  cars: Observable<Car[]>;
 
-  ngOnInit(): void {
+  constructor(private carService: CarService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.cars = this.carService.getCarsList();
+  }
+
+  deleteCar(id: number) {
+    this.carService.deleteCar(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
+  carCoordinations(id: number) {
+    this.router.navigate(['coordinations', id]);
   }
 
 }
