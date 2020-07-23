3e4775f348a5df4627ba7aa481a097bd016feacd
Mazloom <rmazloom88@gmail.com>
2020-07-23 22:35:51 +0430

    add components and necessary modules and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import { CreateCarComponent } from './create-car/create-car.component';
-import { CarCoordinationsComponent } from './car-coordinations/car-coordinations.component';
-import { CarListComponent } from './car-list/car-list.component';
-import { UpdateCarComponent } from './update-car/update-car.component';
+import {CreateCarComponent} from './create-car/create-car.component';
+import {CarCoordinationsComponent} from './car-coordinations/car-coordinations.component';
+import {CarListComponent} from './car-list/car-list.component';
+import {UpdateCarComponent} from './update-car/update-car.component';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app.routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { UpdateCarComponent } from './update-car/update-car.component';
     UpdateCarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
