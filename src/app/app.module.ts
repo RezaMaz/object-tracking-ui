a4249a7b020a9ef92df68ae10c7a17fefc995d64
Mazloom <rmazloom88@gmail.com>
2020-07-23 15:58:31 +0430

    add component for updating car

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import {AppComponent} from './app.component';
 import { CreateCarComponent } from './create-car/create-car.component';
 import { CarCoordinationsComponent } from './car-coordinations/car-coordinations.component';
 import { CarListComponent } from './car-list/car-list.component';
+import { UpdateCarComponent } from './update-car/update-car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateCarComponent,
     CarCoordinationsComponent,
-    CarListComponent
+    CarListComponent,
+    UpdateCarComponent
   ],
   imports: [
     BrowserModule
