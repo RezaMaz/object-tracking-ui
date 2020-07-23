89d1e04a71cdf39063f3448afca599515867e006
Mazloom <rmazloom88@gmail.com>
2020-07-24 00:25:17 +0430

    fix bugs that prevent car to create

--- src/app/create-car/create-car.component.ts
+++ src/app/create-car/create-car.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Pipe, PipeTransform} from '@angular/core';
 import {Car} from '../model/car';
 import {CarService} from '../car.service';
 import {Router} from '@angular/router';
+import {Coordination} from '../model/coordination';
+import {CarType} from '../model/carType';
 
 @Component({
   selector: 'app-create-car',
@@ -10,24 +12,34 @@ import {Router} from '@angular/router';
 })
 export class CreateCarComponent implements OnInit {
 
-  car: Car = new Car();
-  submitted = false;
+  car: Car;
+  coordination: Coordination;
+  submitted: boolean;
+
+  carTypes = CarType;
+  keys: any[];
 
   constructor(private carService: CarService,
               private router: Router) {
+    this.keys = Object.keys(this.carTypes).filter(k => !isNaN(Number(k)));
   }
 
   // tslint:disable-next-line:typedef
   ngOnInit() {
+    this.newCar();
   }
 
   newCar(): void {
     this.submitted = false;
     this.car = new Car();
+    this.car.coordinations = [];
+    this.coordination = new Coordination();
   }
 
   // tslint:disable-next-line:typedef
   save() {
+    console.log(this.car.coordinations);
+    this.car.coordinations.push(this.coordination);
     this.carService.createCar(this.car)
       .subscribe(data => console.log(data), error => console.log(error));
     this.newCar();
